0e213223ba56671a5275ca78643b4ef96518089c
Masibo Mabele <masibomabele2015@gmail.com>
2023-06-16 11:01:52 +0300

    logout django system added

--- Auth_App/views.py
+++ Auth_App/views.py
@@ -19,5 +19,11 @@ def login_user(request):
             return redirect('login')
     else:
         return render(request, 'authenticate/login.html', {})
-     
+    
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ('you were looged out')) 
+    return redirect('home')
  
--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
+from django.contrib.auth.decorators import login_required
 from . import views
 from api.models import ProductResource
-
 product_resource = ProductResource()
+
+# @login_required
+
 urlpatterns = [
    path('', views.home, name='home'),
    path('products/', views.products, name='products'),
